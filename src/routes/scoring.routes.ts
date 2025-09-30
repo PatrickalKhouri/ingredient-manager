ca5ebdf9d05dda1e2123441aa4c777d612d57f1a
Patrick Khouri <patrick.darya@gmail.com>
2025-09-30 15:32:12 -0300

    comenting for build

--- src/routes/scoring.routes.ts
+++ src/routes/scoring.routes.ts
@@ -6,10 +6,11 @@ const router = Router();
 /**
  * POST /v1/scoring/rules/R01/evaluate
  * Body: { product_id: string }
- * - Computes Rule 1 for the given product
+ * - Computes Rule 1 for the given product 
  * - Saves into products_scores.rules[rule_index:1]
  * - Recomputes total_score
  */
+
 router.post('/v1/scoring/rules/R01/evaluate', ScoringController.evaluateRule1);
 
 export default router;
